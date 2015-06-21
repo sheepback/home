ca4f7b5ec2f56cedb64e92d38ffff2a8473b21d5
Alex <Alex@Alex-PC.multi.box>
2015-06-21 21:09:01 +0200

    added IO

--- de.sheepback/src/de/sheepback/main/Main.java
+++ de.sheepback/src/de/sheepback/main/Main.java
@@ -1,7 +1,11 @@
 package de.sheepback.main;
 
+import utils.IO;
+
 public class Main {
 	public static void main(String[] args) {
-		
+		String s;
+		s = IO.readString("Eingabe: ");
+		System.out.println(s);
 	}
 }
